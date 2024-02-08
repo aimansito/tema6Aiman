e9757ad8ec1c27447d0df640600e6f600ff5a346
Aiman <ahardao1001@g.educaand.es>
2024-02-08 09:51:50 +0100

    suma y mostrar con re

--- src/main/java/recursividad/Ej1.java
+++ src/main/java/recursividad/Ej1.java
@@ -13,11 +13,30 @@ public class Ej1 {
     //2 sumar de forma recursiva los elementos de un array de la clase carrito
     
     public static void main(String[] args) {
+        int contador = 0;
         int [] array = new int[5];
         array[0]=4;
         array[1]=3;
         array[2]=5;
         array[3]=7;
         array[4]=8;
+//        imprimirArray(array, contador);
+        System.out.println(sumarArray(array, contador));
+    }
+    public static void imprimirArray(int[] array, int contador){
+        if(array.length>contador){
+            System.out.println(array[contador]);
+            imprimirArray(array, contador+1);
+        }else{
+            System.out.println("se termino");
+        }
+    }
+    public static int sumarArray(int[] array,int contador){
+        int suma = 0;
+        if(array.length>contador){
+            suma += array[contador];
+            suma += sumarArray(array, contador+1);
+        }
+        return suma;
     }
 }
