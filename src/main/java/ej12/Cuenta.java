ca6b28a2cae477b4e8ed580dd1b400b765fac6e4
Aiman <ahardao1001@g.educaand.es>
2024-02-16 00:06:03 +0100

    figura

--- src/main/java/ej12/Cuenta.java
+++ src/main/java/ej12/Cuenta.java
@@ -21,5 +21,43 @@ public class Cuenta {
         this.saldo = 0;
         this.cliente = cliente;
     }
+
+    public Cuenta() {
+    }
+
+    public String getNumeroCuenta() {
+        return numeroCuenta;
+    }
+
+    public void setNumeroCuenta(String numeroCuenta) {
+        this.numeroCuenta = numeroCuenta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public Persona getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Persona cliente) {
+        this.cliente = cliente;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Cuenta{");
+        sb.append("numeroCuenta=").append(numeroCuenta);
+        sb.append(", saldo=").append(saldo);
+        sb.append(", cliente=").append(cliente);
+        sb.append('}');
+        return sb.toString();
+    }
     
 }
